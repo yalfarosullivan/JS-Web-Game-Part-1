ede99a54c2744701c40b4edadb796f3d0f315c82
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-10-02 20:36:26 -0700

    Added rest of New Image

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 
-function newImage (url, left, bottom){
-let object = document.createElement('img')
-object.src = url
-object.style.position = 'fixed'
-object.style.left = left +'px'
-object.style.bottom = bottom + 'px'
-document.body.append(object)
-return object
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
 }
 
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
 
-newImage('assets/green-character.gif', 100., 100)
-newImage('assets/pine-tree.png', 200, 300)
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
