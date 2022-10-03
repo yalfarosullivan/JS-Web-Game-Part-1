a08598f70168a9b9ca4ce0716729af1816e34f4f
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-10-02 20:16:19 -0700

    Added New Image

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage (url, left, bottom){
+let object = document.createElement('img')
+object.src = url
+object.style.position = 'fixed'
+object.style.left = left +'px'
+object.style.bottom = bottom + 'px'
+document.body.append(object)
+return object
+}
+
+
+newImage('assets/green-character.gif', 100., 100)
+newImage('assets/pine-tree.png', 200, 300)
